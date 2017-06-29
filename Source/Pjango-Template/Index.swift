b57f8642ca310ec48bd221462b0867d9e64b3ff2
enums <enum@enumsblog.com>
2017-06-29 23:30:56 +0800

    Add copyright info

--- Source/Pjango-Template/Index.swift
+++ Source/Pjango-Template/Index.swift
@@ -1,9 +1,9 @@
 //
 //  Index.swift
-//  Pjango
+//  Pjango-Template
 //
 //  Created by 郑宇琦 on 2017/6/18.
-//
+//  Copyright © 2017年 郑宇琦. All rights reserved.
 //
 
 import Foundation
--- Source/Pjango-Template/main.swift
+++ Source/Pjango-Template/main.swift
@@ -1,9 +1,9 @@
 //
 //  main.swift
-//  Pjango-Demo
+//  Pjango-Template
 //
 //  Created by 郑宇琦 on 2017/6/18.
-//
+//  Copyright © 2017年 郑宇琦. All rights reserved.
 //
 
 import Foundation
